79926529e3cab29aa81748ad337fbcab377d4f73
newtalentxp <newtalentxp@126.com>
2020-09-08 19:35:25 +0800

    fix

--- index-server/app/http/middlewares/global.go
+++ index-server/app/http/middlewares/global.go
@@ -1,30 +1,24 @@
 package middlewares
 
 import (
-	"github.com/aviddiviner/gin-limit"
-	"github.com/danielkov/gin-helmet"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/objforce/objforce/index-server/config"
-	"github.com/thinkerou/favicon"
+	"github.com/opentracing/opentracing-go"
+	"github.com/xxxmicro/base/http/gin/middlewares"
 )
 
-func GlobalMiddlewares(server *gin.Engine, config *config.AppConfig, logMiddleware *LogMiddleware) {
+func GlobalMiddlewares(server *gin.Engine, config *config.AppConfig, logMiddleware *LogMiddleware, tracer opentracing.Tracer) {
+	promMonitor := NewPrometheusMonitor("objforce", "com.xapis.api.index")
+
 	server.Use(
 		logMiddleware.Handler(),
 		cors.Default(),
-		limit.MaxAllowed(config.Connection),
+		// limit.MaxAllowed(config.Connection),
 		gzip.Gzip(gzip.DefaultCompression),
-		helmet.NoSniff(),
-		helmet.DNSPrefetchControl(),
-		helmet.FrameGuard(),
-		helmet.SetHSTS(true),
-		helmet.IENoOpen(),
-		helmet.XSSFilter(),
-		helmet.NoCache(),
-		favicon.New("./public/favicon.ico"),
-		static.Serve("/", static.LocalFile("./public", false)),
+		// traceMiddleware.Handler(),
+		middlewares.Middleware(tracer),
+		promMonitor.PromMiddleware(),
 	)
 }
