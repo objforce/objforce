3b789eb20663024bc5e9d3a174bb83ba949b70a2
newtalentxp <52388483@qq.com>
2020-09-10 22:59:54 +0800

    fix

--- meta-api/app/http/controllers/api.controller.go
+++ meta-api/app/http/controllers/api.controller.go
@@ -1,16 +1,58 @@
 package controllers
 
-import(
-	"net/http"
+import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type APIController struct{}
+
+type ActionHandler func (ctx *gin.Context)
+
+type APIController struct{
+	actionHandlers map[string]ActionHandler
+}
 
 func (c *APIController) Ping(ctx *gin.Context) {
 	ctx.String(http.StatusOK, "pong")
 }
 
+func (c *APIController) Call(ctx *gin.Context) {
+	action := ctx.Param("action")
+	if len(action) == 0 {
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	params := map[string]interface{}{}
+	if err := ctx.BindJSON(params); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	handler, ok := c.actionHandlers[action]
+	if !ok {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{"code": -1, "msg": fmt.Sprintf("handler %s not exists", action)})
+		return
+	}
+
+	handler(ctx)
+}
+
 func NewAPIController() *APIController {
-	return &APIController{}
+	c := APIController{}
+
+	actionHandlers := make(map[string]ActionHandler)
+	actionHandlers["metadata.describe"] = c.CreateMetadata
+	actionHandlers["metadata.describe"] = c.DescribeMetadata
+	actionHandlers["metadata.list"] = c.ListMetadata
+	actionHandlers["metadata.read"] = c.ReadMetadata
+	actionHandlers["metadata.rename"] = c.RenameMetadata
+	actionHandlers["metadata.update"] = c.UpdateMetadata
+	actionHandlers["metadata.upsert"] = c.UpsertMetadata
+	actionHandlers["metadata.delete"] = c.DeleteMetadata
+
+	c.actionHandlers = actionHandlers
+
+	return &c
 }
